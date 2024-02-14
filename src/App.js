5d87d42f14974d0d5bfacdd667ff50f7179a80fd
unknown <hichamsskouti@gmail.com>
2024-02-14 21:49:42 +0100

    last

---
+++ src/App.js
@@ -0,0 +1,212 @@
+import React from "react";
+import "./App.css";
+
+import colors from "./utils/colors";
+import Screen from "./components/Screen";
+
+import ursinho_pandinha_ok from "./assets/ursinho_pandinha_ok.gif";
+import ursinho_estudando from "./assets/ursinho_estudando.gif";
+import pandinha_brava from "./assets/pandinha_fofa_cara_de_brava.gif";
+import pandinha from "./assets/pandinha.gif";
+import ursinho_S2_pandinha from "./assets/ursinho_S2_pandinha.gif";
+import ursinha_chorando from "./assets/ursinha_chorando.gif";
+import ursinho_chorando from "./assets/ursinho_chorando.gif";
+import pandinha_martelo from "./assets/pandinha_martelo.png";
+import ursinho_catucando_pandinha from "./assets/ursinho_catucando_pandinha.gif";
+import ursinho_coracao from "./assets/ursinho_coracao.jpg";
+import pandinha_coracao from "./assets/pandinha_coracao.gif";
+import ursinhos_apaixonados from "./assets/ursinhos_apaixonados.gif";
+import ursinhos_caminhada from "./assets/ursinhos_caminhada.jpg";
+import ursinhos_fotinha from "./assets/ursinhos_fotinha.jpg";
+import ursinhos_bebida from "./assets/ursinhos_bebida.gif";
+import ursinha_mordidas from "./assets/ursinha_mordidas.gif";
+import w from "./assets/WhatsApp Image 2024-02-14 at 7.38.06 PM (1).jpeg";
+import w1 from "./assets/WhatsApp Image 2024-02-14 at 7.38.06 PM (2).jpeg";
+import w2 from "./assets/WhatsApp Image 2024-02-14 at 7.37.48 PM.jpeg";
+import w3 from "./assets/imgga.jpeg";
+import w4 from "./assets/WhatsApp Image 2024-02-14 at 7.37.49 PM.jpeg";
+
+const App = () => {
+
+  return (
+    <div className="main">
+      <Screen
+        imageUrl={ursinho_pandinha_ok}
+        bgColor={colors.red}
+        textColor={colors.white}
+        title="lkbdbd"
+        text="this is About The love of my life lkbded the best ever happend to me and best thing will ever happend ... So buckel up and Haya Bina"
+        btnText="Doooooooooooooooz ❤️❤️"
+        sequency="1"
+      />
+      <Screen
+        imageUrl={ursinho_estudando}
+        bgColor={colors.blue}
+        textColor={colors.white}
+        title="7yaaaaaaati ❤️"
+        text=" My first impression when i first saw I really thought you re gorgeous and beautiful 
+        But I didn't talk to you from the start until after I saw that you also are funny and have an amazing personality and we share the same vibe . At the beginning I thought to my self why not talk to her she is amazing why not at least having her as a friend so I tried to talk to you many times but there always was something off."
+        btnText="Ara lakher  ❤️❤️"
+        sequency="2"
+      />
+      <Screen
+        imageUrl={pandinha}
+        bgColor={colors.pink}
+        textColor={colors.white}
+        title="Lghzaaaaaala ❤️❤️"
+        text="until that day when we had the English class when I asked for your number as an excuse I said I will send you some c++ and python courses you also took my number and you saved it as python. At that moment I lost hope cus u saved as python I said comon at least diri name or something hahaha after that we start talking now and then ..."
+        btnText="Pythoooon ><"
+        sequency="3"
+      />
+      <Screen
+        imageUrl={ursinho_S2_pandinha}
+        bgColor={colors.red}
+        textColor={colors.white}
+        title="Ti amo ❤️❤️"
+        text="It was really weird back then to pretend like I did not know you so they do feel bad . The one time you said you were done with relationships and after that you said people misunderstood ur vibe and thought you were into theme and that made think like did I misunderstood ur vibe and cus you said your done with relationships so there was no hope for me . I didn't know what to do so I said why not leave it like this ur an amazing person why not just being a friend with you . After a while a found my self drowning in love with you after we talked alot . Then there was that reel about lkbdbd and wink ."
+        btnText="Ara Lakher ❤️❤️❤️❤️"
+        sequency="4"
+      />
+      <Screen
+        imageUrl={w3}
+        bgColor={colors.purple}
+        textColor={colors.white}
+        title="wlwlwlwlwlwlwl ❤️❤️❤️❤️"
+        text="we start setting at the back on English class at that point I started to realize that I can't keep my eyes away from you . Like I can look at you all that cus ur amazingly beautiful . It went like that until that day I said I love you and I went out . I was a bit nervous then cus you said you re Done with relationships and you were scared of them"
+        btnText="Continuar"
+        sequency="5"
+      />
+      <Screen
+        imageUrl={w4}
+        bgColor={colors.green}
+        textColor={colors.white}
+        title="Zin dl3alam  ❤️❤️❤️❤️"
+        text="When I came back I acted like nothing happened until we brought it up and I said that I really liked you and we made it official and from then my life got better cus ur a part of it . And cus I have an amazing girlfriend like you. :)"
+        btnText="Zib Balak ❤️❤️❤️❤️"
+        sequency="6"
+      />
+      <Screen
+        imageUrl={pandinha_martelo}
+        bgColor={colors.blue}
+        textColor={colors.white}
+        title="Hyattiii "
+        text="Hadi nti ach Kat9l9i hehehehe no way You look goargous fach katk3ay okangolik chi haja okad7ki ..."
+        btnText="Ami 9Lbiiiiiiii ❤️❤️❤️❤️"
+        sequency="7"
+      />
+      <Screen
+        imageUrl={pandinha_brava}
+        bgColor={colors.yellow}
+        textColor={colors.white}
+        title="kantsta 3LIK ALGHZALA ❤️❤️❤️❤️ "
+        text=" HAHAHAHAHAHAHHA LA LA HADI HIYA FACH KAT9L9I HEHEHEHE OKANGOLIK MALK katgoli liya wallo hahahahaa "
+        btnText="Ara limnb3d ❤️❤️❤️❤️"
+        sequency="8"
+      />
+      <Screen
+        imageUrl={ursinho_catucando_pandinha}
+        bgColor={colors.purple}
+        textColor={colors.white}
+        title="zin dyali "
+        text="My Sweet
+        I want you to know how much you mean to me. Your beauty, 
+        When I see you, I can't help but smile. Your smile and your eyes are so special, and they make me really happy. Your smile has a way of making my heart skip a beat ."
+        btnText="Love love love ❤️"
+        sequency="9"
+      />
+      <Screen
+        imageUrl={ursinho_coracao}
+        bgColor={colors.blue}
+        textColor={colors.white}
+        title="Ur eyes alghzala"
+        text="how much I love your eyes. They're so beautiful and special. When you're happy, they sparkle and it's the best thing ever. Looking into your eyes makes me feel really happy and connected to you. really a lghzala ur eyes chi 7aja lahoma barik wlwlwlwlwlw kanmot ana "
+        btnText="Owwwwn <3"
+        sequency="10"
+      />
+      <Screen
+        imageUrl={pandinha_coracao}
+        bgColor={colors.yellow}
+        textColor={colors.white}
+        title="wlwlw a7yati  ❤️ "
+        text="and ur lips a 7yati 💋💋💋 they are lipsing wlwlwlwl  fach kadiri lihom vasling chi nhar anti7 lik tmak hahahahahahhahaa"
+        btnText="OkoKoKokOKokOKok"
+        sequency="11"
+      />
+      <Screen
+        imageUrl={ursinhos_apaixonados}
+        bgColor={colors.red}
+        textColor={colors.white}
+        title="EVRTHING ABOUT YOU"
+        text="gals kanfker f hb 9lbi cus you never left my mind and because ofc when I can't sleep I start thinking about you  and I start thinking how much i love you cus you really can't imagine how deep my love is . Kanbghik hta sf "
+        btnText="okokOKokOkK "
+        sequency="12"
+      />
+      <Screen
+        imageUrl={ursinhos_caminhada}
+        bgColor={colors.pink}
+        textColor={colors.white}
+        title="WINK WINK WINK ❤️ "
+        text=" IWA Azin mazal mabghiti tgoli liya kikadiri Liha kol Mera nchofk katwli ghezaala 3la Lowl huuum bizzaaar hadchi ahbiba. Evrytime I see you Azin I keep wondering how I got lucky to have such a beautiful girl in my life cus everytime I see you its like the first time I saw you "
+        btnText="ESTAY LOCO LALALALALALALAL"
+        sequency="13"
+      />
+      <Screen
+        imageUrl={ursinhos_fotinha}
+        bgColor={colors.green}
+        textColor={colors.white}
+        title="Lkbdb <3 <3 <3"
+        text=" I honestly can't stop looking at you and admiring how gorgeous you are. I always tell you how much i love your eyes cus they are hypnotizing , wlwlwlwl lahiydti ndader ana rah i became speechless . fach katgoli matchofch fiya o wakha hkak kanchof hahahahahahah "
+        btnText="ila wrkti athydi ndader gheda hahahahah  ❤️ "
+        sequency="14"
+      />
+      <Screen
+        imageUrl={ursinhos_bebida}
+        bgColor={colors.purple}
+        textColor={colors.white}
+        title="hehehehehhehe  ❤️"
+        text="rani mansach gheda athydihom mafihach haahahahahahahhahahaha "
+        btnText="Last chance if you click athiydihom ..."
+        sequency="15"
+      />
+      <Screen
+        imageUrl={ursinha_mordidas}
+        bgColor={colors.blue}
+        textColor={colors.white}
+        title="my preety laddy "
+        text="I am really deeply in love with you you got me thinking about you all the time "
+        btnText="Wink wink Wink"
+        sequency="16"
+      />
+      <Screen
+        imageUrl={w}
+        bgColor={colors.purple}
+        textColor={colors.white}
+        title="JUST AMAzINGGGGGG "
+        text="And when you smile, the whole world stops and stares for a while..."
+        btnText="Stop the world babe"
+        sequency="17"
+      />
+      <Screen
+        imageUrl={w1}
+        bgColor={colors.blue}
+        textColor={colors.white}
+        title="I'm so in love with you"
+        text="I can hear her heartbeat for a thousand miles, and the heavens open every time she smiles.."
+        btnText=">>>>>>>>>>>>>>>"
+        sequency="18"
+      />
+      <Screen
+        imageUrl={w2}
+        bgColor={colors.red}
+        textColor={colors.white}
+        title="lzghala"
+        text="Finalmente i  really love you alghzala vraiment kanmot 3link maymknch how happy i am when i am with you . kanstsa 3lik alghzala chi haja lahoma barik 
+        I hope you liked it. I love you so much ahbiba  ~Lkbdbd"
+        btnText="9te3 laYdir lkhir"
+        sequency="19"
+      />
+    </div>
+  );
+};
+
+export default App;
